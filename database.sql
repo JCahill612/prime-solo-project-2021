8e8d7551e24e7c4e3495457fbaad59fc31309c4e
jcahill612 <jenny.e.cahill@gmail.com>
2021-12-24 08:57:43 -0600

    updated database

--- database.sql
+++ database.sql
@@ -4,25 +4,43 @@
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
 
-CREATE TABLE "user" (
+CREATE TABLE "user"
+(
     "id" SERIAL PRIMARY KEY,
-    "username" character varying(80) NOT NULL UNIQUE,
-    "password" character varying(1000) NOT NULL,
-    "user_email" character varying(255),
-    "user_location" character varying(80),
-    "user_date" date DEFAULT CURRENT_DATE,
-    "create_date" date
+    "username" varchar(100),
+    "password" varchar(255),
+    "user_email" varchar(255),
+    "user_img" varchar(1000),
+    "user_location" varchar(255),
+    "user_date" DATE DEFAULT CURRENT_DATE
 );
 
-CREATE TABLE post (
-    "post_id" integer DEFAULT nextval('toys_id_seq'::regclass) PRIMARY KEY,
-    "post_name" character varying(240),
-    "post_body" character varying(360),
-    "post_image" character varying(1000),
-    "post_date" character varying(80),
-    "user_id" character varying(80),
-    FOREIGN KEY (user_id) REFERENCES "user"(id),
+
+CREATE TABLE "category"
+(
+    "cat_id" SERIAL PRIMARY KEY NOT NULL,
+    "cat_name" varchar(25) NOT NULL,
+    "cat_description" varchar(255) NOT NULL
 );
+
+CREATE TABLE "post"
+(
+    "post_id" SERIAL PRIMARY KEY NOT NULL,
+    "post_name" varchar(100) NOT NULL,
+    "post_body" varchar(5000) NOT NULL,
+    "post_image" varchar(1000) NOT NULL,
+    "post_date" DATE NOT NULL DEFAULT CURRENT_DATE,
+    "post_cat" integer NOT NULL,
+    "user_id" integer NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES "user"(id),
+    FOREIGN KEY (post_cat) REFERENCES "category"(cat_id)
+);
+
+INSERT INTO "category" ('cat_name', 'cat_description')
+VALUES
+    ('Buy', 'Buyers Category'),
+    ('Sell', 'Sellers Category'),
+    ('Trade', 'Traders Category');
 
 CREATE TABLE message (
     "id" integer DEFAULT nextval('messages_id_seq'::regclass) PRIMARY KEY,
