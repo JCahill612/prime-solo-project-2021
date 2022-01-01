a6d923862ad84cd29f5a208ab5e6fa26cba0e350
jcahill612 <jenny.e.cahill@gmail.com>
2022-01-01 12:21:56 -0600

    changing landing page

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -51,16 +51,16 @@ function LandingPage() {
             amet nisi.
           </p>
         </div>
-        <div className="grid-col grid-col_4">
-          <RegisterForm />
+        {/* <div className="grid-col grid-col_4">
+          <RegisterForm /> */}
 
           <center>
-            <h4>Already a Member?</h4>
+            {/* <h4>Already a Member?</h4>
             <button className="btn btn_sizeSm" onClick={onLogin}>
               Login
-            </button>
-          </center>
-        </div>
+            </button> */}
+          </center> 
+         {/* </div> */}
       </div>
     </div>
   );
