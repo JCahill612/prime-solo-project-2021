3d7234bbcb6b84bd07884f451ef923d39a2435f4
jcahill612 <jenny.e.cahill@gmail.com>
2022-01-01 22:50:16 -0600

    fixing trade request

--- src/components/TradeRequests/TradeRequests.jsx
+++ src/components/TradeRequests/TradeRequests.jsx
@@ -217,10 +217,10 @@ export default function TradeOffers() {
     
           <TableHead>
               <TableRow>
-                <TableCell>Requested Toy</TableCell>
-                <TableCell >Requested Toy Image</TableCell>
-                <TableCell >Offered Toy</TableCell>
+                <TableCell>Offered Toy</TableCell>
                 <TableCell >Offered Toy Image</TableCell>
+                <TableCell >Requested Toy</TableCell>
+                <TableCell >Requested Toy Image</TableCell>
                 <TableCell >Status</TableCell>
                 <TableCell >Message</TableCell>
                 <TableCell >Reject Offer</TableCell> 
