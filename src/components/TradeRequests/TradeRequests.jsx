7bbb4a23aaa4366ce5a079cfbeeab5a8e2d70152
jcahill612 <jenny.e.cahill@gmail.com>
2022-01-03 20:26:58 -0600

    correcting labels for trade requests

--- src/components/TradeRequests/TradeRequests.jsx
+++ src/components/TradeRequests/TradeRequests.jsx
@@ -218,10 +218,10 @@ export default function TradeOffers() {
           <TableHead>
           <h3>Trade Requests</h3>
               <TableRow>
-                <TableCell>Requested Toy</TableCell>
-                <TableCell >Requested Toy Image</TableCell>
-                <TableCell >Offered Toy</TableCell>
+                <TableCell>Offered Toy</TableCell>
                 <TableCell >Offered Toy Image</TableCell>
+                <TableCell >Requested Toy</TableCell>
+                <TableCell >Requested Toy Image</TableCell>
                 <TableCell >Status</TableCell>
                 <TableCell >Message</TableCell>
                 <TableCell >Reject Offer</TableCell> 
