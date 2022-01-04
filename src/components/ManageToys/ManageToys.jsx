4e94e1f3a5bc3e5746191998ec4b92db1a50e975
jcahill612 <jenny.e.cahill@gmail.com>
2022-01-04 09:59:45 -0600

    adding user info to manage toys page

--- src/components/ManageToys/ManageToys.jsx
+++ src/components/ManageToys/ManageToys.jsx
@@ -162,11 +162,12 @@ export default function ManageToys() {
     <Box component="span"
     sx={{ display: 'inline-block', mx: '5px', transform: 'scale(.9)' }}>
     <h2>Welcome, {user.username}!</h2>
-    <p>Street Address: {user.user_address}</p>
+    <p>Address: {user.user_address}</p>
     <p>Zip Code: {user.zip_code}</p>
   </Box>
       <Table sx={{ minWidth: 500 }} aria-label="custom pagination table">
       <TableHead>
+          <h4>Your Toys</h4>
           <TableRow>
             <TableCell>Toy Name</TableCell>
             <TableCell >Image</TableCell>
