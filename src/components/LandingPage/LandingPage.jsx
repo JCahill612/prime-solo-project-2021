e4a419f2657277d03f4b7329a3f3096450c9386d
jcahill612 <jenny.e.cahill@gmail.com>
2022-01-08 23:33:41 -0600

    adding styling

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -20,10 +20,14 @@ function LandingPage() {
       <div className="grid">
         <div className="grid-col grid-col_8">
 
-          <h4>
-          A place to trade blind bag/surprise toys to complete your collections faster. 
+          <h4>Blind bags (also referred to as surprise toys) involve small and collectible toys hidden inside packaging of some. There are now hundreds of different blind bag lines, from old favorites such as My Little Pony, Transformers and Teenage Mutant Ninja Turtles to new fads like Tokidoki, Zomlings and the hugely popular L.O.L. Surprise! dolls.</h4>
+          <h4>Since you dont know which surprise toy you will get, you might not get the toy you wanted...
+
+    </h4>
+          <h3>
+          Enter Blind Bag Market! A place to trade blind bag/surprise toys to complete your collections faster. 
           <p></p>
-          </h4>
+          </h3>
           <p>
           <h3>Getting started is easy!</h3>
       <p></p>
